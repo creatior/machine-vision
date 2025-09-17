3964ef8696f37cb4514b182f15e2ab8f6ded0b6a
creatior <vartem2020@mail.ru>
2025-09-17 23:13:41 +0300

    L1T3 video loading

--- lab1/main.py
+++ lab1/main.py
@@ -1,8 +1,23 @@
 import cv2 as cv
-img = cv.imread("lab1\dog.jpg", cv.IMREAD_GRAYSCALE)
-resized = cv.resize(img, (800, 600))
+# img = cv.imread("lab1\dog.jpg", cv.IMREAD_COLOR)
+# resized = cv.resize(img, (800, 600))
 
-cv.namedWindow("output", cv.WINDOW_AUTOSIZE)
-cv.imshow("output", resized)
+# cv.namedWindow("output", cv.WINDOW_AUTOSIZE)
+# cv.imshow("output", resized)
+# cv.waitKey(0)
 
-cv.waitKey(0)
+cap = cv.VideoCapture("lab1/dance.mov")
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2Lab)   
+    cv.imshow("Video", gray)
+
+    if cv.waitKey(25) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv.destroyAllWindows()
