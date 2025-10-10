b34db4ea7ae3b5c613a5803420fba11e889d1ecb
creatior <vartem2020@mail.ru>
2025-10-10 18:04:31 +0300

    L2T2 red mask fixed

--- lab2/main.py
+++ lab2/main.py
@@ -1,7 +1,6 @@
 import cv2
 import numpy as np
 
-# Открываем камеру (0 - первая подключенная камера)
 cap = cv2.VideoCapture(1)
 
 while True:
@@ -9,16 +8,30 @@ while True:
     if not ret:
         break
 
+    # Преобразуем изображение в HSV
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    lower_red = np.array([0, 0, 0])
-    upper_red = np.array([40, 40, 255])
+    # Диапазоны для красного цвета
+    lower_red1 = np.array([0, 140, 60])
+    upper_red1 = np.array([10, 255, 255])
 
-    mask = cv2.inRange(hsv, lower_red, upper_red)
+    lower_red2 = np.array([170, 140, 60])
+    upper_red2 = np.array([180, 255, 255])
 
+    # Создаем две маски и объединяем их
+    mask1 = cv2.inRange(hsv, lower_red1, upper_red1)
+    mask2 = cv2.inRange(hsv, lower_red2, upper_red2)
+    mask = cv2.bitwise_or(mask1, mask2)
+
+    # Применяем маску к оригинальному изображению
+    red_only = cv2.bitwise_and(frame, frame, mask=mask)
+
+    # Показываем результат
     cv2.imshow("Original", frame)
     cv2.imshow("Threshold (Red mask)", mask)
+    cv2.imshow("Filtered (Red Only)", red_only)
 
+    # Нажми ESC для выхода
     if cv2.waitKey(1) & 0xFF == 27:
         break
 
