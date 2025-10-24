7077efbd5d800661a9e83a3d6b05ea5d096dee1b
creatior <vartem2020@mail.ru>
2025-10-24 18:32:55 +0300

    L3T3 gaussian filter

--- lab3/main.py
+++ lab3/main.py
@@ -1,4 +1,8 @@
+import cv2
 import numpy as np
+
+KERNEL_SIZE = 7
+BLUR_PARAM = 20
 
 def gaussian_kernel(size, sigma):
     center = size // 2
@@ -7,9 +11,27 @@ def gaussian_kernel(size, sigma):
     g /= np.sum(g)
     return g
 
-for size in [3, 5, 7]:
-    sigma = 5  
+def gaussian_filter(image, size=5, sigma=1.0):
     kernel = gaussian_kernel(size, sigma)
-    print(f"\nМатрица Гаусса {size}x{size}")
-    print(np.round(kernel, 4))
-    print(f"Сумма элементов: {np.sum(kernel)}")
+    height, width = image.shape
+    pad = size // 2
+
+    padded = cv2.copyMakeBorder(image, pad, pad, pad, pad, cv2.BORDER_REFLECT)
+
+    result = np.zeros_like(image, dtype=float)
+
+    for i in range(height):
+        for j in range(width):
+            region = padded[i:i+size, j:j+size]
+            result[i, j] = np.sum(region * kernel)
+
+    result = np.clip(result, 0, 255).astype(np.uint8)
+    return result
+
+img = cv2.imread('images/mole.jpg', cv2.IMREAD_GRAYSCALE)
+blurred = gaussian_filter(img, size=KERNEL_SIZE, sigma=BLUR_PARAM)
+
+cv2.imshow('Исходное изображение', img)
+cv2.imshow('После фильтра Гаусса', blurred)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
